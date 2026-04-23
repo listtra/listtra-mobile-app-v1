0b81594c511e4fe9e3fa0b206ab8f7fdeb9e1d9d
jibin-dev71 <jibin.babu@innovinlabs.com>
2026-04-23 21:15:02 +0530

    chore: bump build number and version code to 27

--- app.config.js
+++ app.config.js
@@ -20,7 +20,7 @@ export default {
       entitlements: {
         "com.apple.developer.applesignin": ["Default"],
       },
-      buildNumber: "26",
+      buildNumber: "27",
       supportsTablet: true,
       statusBarStyle: "dark-content",
       statusBarBackgroundColor: "#ffffff",
@@ -91,7 +91,7 @@ export default {
           category: ["BROWSABLE", "DEFAULT"],
         },
       ],
-      versionCode: 26,
+      versionCode: 27,
       edgeToEdgeEnabled: false,
       statusBarStyle: "dark-content",
       softInputMode: "adjustResize",
