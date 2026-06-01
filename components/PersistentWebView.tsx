862caf8a7631dc29f3f56ab51ac04f36331b79a7
jibin-dev71 <jibin.babu@innovinlabs.com>
2026-06-01 13:08:33 +0530

    chore: update API URL for production in PersistentWebView and bump Expo dependencies in Podfile.lock

--- components/PersistentWebView.tsx
+++ components/PersistentWebView.tsx
@@ -27,7 +27,7 @@ import OfflineScreen from "./OfflineScreen";
 
 const BASE_URL = __DEV__
   ? "https://staging.zirkly.com"
-  : "https://www.zirkly.com.au";
+  : "https://staging.zirkly.com";
 
 export interface PersistentWebViewRef {
   refresh: () => void;
